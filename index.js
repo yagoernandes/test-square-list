21568bc789403f1c1059269865e1f830aa1282d9
Yago Ernandes <yago.dourado@tivit.com>
2022-05-06 09:58:31 -0300

    fix:validating if input is number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function squareRootMap(list){
     if(Object.prototype.toString.call(list) !== '[object Array]') throw new Error('Must be an array')
     if(list.length <= 0) throw new Error('Array cannot be empty')
 
-    return list.map(x=> x*x).sort((a,b)=>a-b)
+    return list.filter(x=>Number.isInteger(x)).map(x=> x*x).sort((a,b)=>a-b)
 }
 
 module.exports = squareRootMap
